e9efbf1a5ab934aaec85645eb33222100b99e282
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2022-11-09 14:04:28 +0100

    tilføjelse af besked

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let firstName;
 let telefon;
+let besked;
 
 document.getElementById("minKnap").onclick = function() {
     firstName = document.getElementById("fname").value;
@@ -7,4 +8,7 @@ document.getElementById("minKnap").onclick = function() {
 
     telefon = document.getElementById("tlf").value;
     document.getElementById("tlfParagraph").innerHTML = telefon;
+
+    besked = document.getElementById("besked").value;
+    document.getElementById("beskedParagraph").innerHTML = besked;
 }
